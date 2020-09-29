5f647ff5403904d8925921546ee602aa00a42d36
Yagdra <43428247+Yagdra-nsk@users.noreply.github.com>
2020-09-30 01:03:00 +0700

    Add Lab15

---
+++ Lab15/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+sem_t *sem1;
+sem_t *sem2;
+
+void *childThread(void *arg) {
+
+    for (int i = 0; i < 10; ++i) {
+        sem_wait(&sem1);
+        printf("Child\n");
+        sem_post(&sem2);
+    }
+
+    pthread_exit((void *) 0);
+}
+
+int main() {
+    sem1 = sem_open("/sem1", O_CREAT | O_EXCL, S_IRWXU, 0);
+    sem2 = sem_open("/sem2", O_CREAT | O_EXCL, S_IRWXU, 1);
+
+    if (fork()) {
+        for (int i = 0; i < 10; ++i) {
+            sem_wait(sem2);
+            printf("Parent\n");
+            sem_post(sem1);
+        }
+    } else {
+        for (int i = 0; i < 10; ++i) {
+            sem_wait(sem1);
+            printf("Child\n");
+            sem_post(sem2);
+        }
+    }
+
+    sem_unlink("/sem1");
+    sem_unlink("/sem2");
+    pthread_exit((void *) 0);
+}
